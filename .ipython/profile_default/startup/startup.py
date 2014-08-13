90204e3bece417577fddae0ea1eda22dca13ef7f
Henry Stokeley <hstokeley@gmail.com>
2014-08-13 14:25:27 +0900

    rename conflicting ipython function debug to debuf

--- .ipython/profile_default/startup/startup.py
+++ .ipython/profile_default/startup/startup.py
@@ -2,7 +2,7 @@ def set_trace():
     from IPython.core.debugger import Pdb
     Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
 
-def debug(f, *args, **kwargs):
+def debugf(f, *args, **kwargs):
     from IPython.core.debugger import Pdb
     pdb = Pdb(color_scheme='Linux')
     return pdb.runcall(f, *args, **kwargs)
