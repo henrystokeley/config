a4a7c84ca2098674f59eedc99d1f53f12d1d8a75
Henry Stokeley <hstokeley@gmail.com>
2015-07-17 11:46:13 +0900

    ipython %debugf magic uses dark background colors

--- .ipython/profile_default/startup/startup.py
+++ .ipython/profile_default/startup/startup.py
@@ -1,8 +1,11 @@
 def set_trace():
     from IPython.core.debugger import Pdb
-    Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
+    import sys
+    # another color scheme for dark backgrounds is 'Linux'
+    Pdb(color_scheme='LightBG').set_trace(sys._getframe().f_back)
+
 
 def debugf(f, *args, **kwargs):
     from IPython.core.debugger import Pdb
-    pdb = Pdb(color_scheme='Linux')
+    pdb = Pdb(color_scheme='LightBG')
     return pdb.runcall(f, *args, **kwargs)
